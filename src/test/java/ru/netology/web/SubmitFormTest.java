a30de3815318765fd0221a087177e5eb90610f33
Perova <av_perova@mail.ru>
2023-08-07 14:17:15 +0700

    Test success

--- src/test/java/ru/netology/web/SubmitFormTest.java
+++ src/test/java/ru/netology/web/SubmitFormTest.java
@@ -9,13 +9,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 class SubmitFormTest {
     @Test
-    void test1() throws InterruptedException {
+    void () throws InterruptedException {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Перова Анастасия");
         form.$("[data-test-id=phone] input").setValue("+79833333333");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=order-success").shouldHave(exactText("Ваша успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
